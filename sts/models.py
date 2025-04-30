e1c550421114acc2ad56f6e92c53fcb1adf4f706
aydossari <aymalmahdi@gmail.com>
2025-04-30 14:17:42 +0300

    Created Customer model

--- sts/models.py
+++ sts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    full_name = models.CharField(max_length=50)
+    
+    # From django docs `https://docs.djangoproject.com/en/4.2/ref/models/fields/#field-options`
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=13)
+    
+    def __str__(self):
+        return self.full_name
