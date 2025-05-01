103bbf1d251161bcf1ff7a986b55877c3d959e49
aydossari <aymalmahdi@gmail.com>
2025-05-01 19:10:29 +0300

    Added ProductDetialView class to handle GET and PATCH for single Product instance

--- sts/views.py
+++ sts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -9,8 +10,8 @@ from .serializers import ProductSerializer
 
 class ProductListCreateView(APIView):
     def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
         return Response(serializer.data, status= 200)
 
     def post(self, request):
@@ -19,3 +20,23 @@ class ProductListCreateView(APIView):
             serializer.save()
             return Response(serializer.data, status= 201)
         return Response(serializer.errors, status= 400)
+    
+    
+class ProductDetilView(APIView):
+    def get_object(self , pk):
+        return get_object_or_404(Product, pk=pk )
+    
+    def get(self,request,pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=200)
+    
+    def patch(self,request,pk):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product ,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer._errors, status= 200)
+        return Response(serializer.errors, status=400)
+    
+    
