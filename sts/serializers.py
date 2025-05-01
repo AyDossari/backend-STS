df5ae2a1152c6b395f6676918df228d6cc18eb9f
aydossari <aymalmahdi@gmail.com>
2025-05-01 16:27:23 +0300

    Added Customer and Driver Serializers

--- sts/serializers.py
+++ sts/serializers.py
@@ -1,3 +1,12 @@
 from rest_framework import serializers
 from .models import *
 
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        
+class DriverSrializer(serializers.ModelSerializer):
+    class Meta:
+        model = Driver
+        fields = '__all__'
