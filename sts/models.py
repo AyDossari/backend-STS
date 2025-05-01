d1b7219c03cfd1c9ed242b5617d9dd1826a90fa8
aydossari <aymalmahdi@gmail.com>
2025-05-01 12:34:01 +0300

    Created Driver Request

--- sts/models.py
+++ sts/models.py
@@ -25,7 +25,7 @@ class Driver(models.Model):
         return self.full_name
     
 class Product(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='products') 
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE, related_name='products') 
     name = models.CharField(max_length=100)
     description = models.TextField()
     weight = models.DecimalField(max_digits=6, decimal_places=2)
@@ -41,3 +41,17 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class DriverRequest(models.Model):
+        driver = models.ForeignKey('Driver', on_delete=models.CASCADE, related_name='requests')
+        product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='driver_driver_requests')
+        
+        # From django docs `https://docs.djangoproject.com/en/4.2/ref/models/fields/#default`
+        status = models.CharField(max_length=20, default='Pending')
+        
+        # From django docs `https://docs.djangoproject.com/en/5.1/ref/models/fields/#datetimefield``
+        created_at = models.DateTimeField(auto_now_add=True)
+        
+        def __str__(self):
+           return f'{self.driver.full_name} , {self.product.name}'
+       
