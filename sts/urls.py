268587e1ed5add89a1b8219bf20da6b1424f1959
aydossari <aymalmahdi@gmail.com>
2025-05-03 19:09:31 +0300

    Improved overall structure and functionality of views

--- sts/urls.py
+++ sts/urls.py
@@ -6,10 +6,15 @@ from .views import (
     CustomerSignupView,
     DriverSignupView,
     CustomerDetilView,
-    DriverDetilView
+    DriverDetilView, 
+    DriverRequestListCreateView
 )
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 @api_view(['GET'])
 def test_api(request):
@@ -19,9 +24,12 @@ urlpatterns = [
     path('api/test/', test_api),
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductDetilView.as_view(), name='product-detail'),
+    path('driver-requests/', DriverRequestListCreateView.as_view(), name='driver-request-create'),
     path('driver-requests/<int:pk>/', DriverRequestDetailView.as_view(), name='driver-request-detail'),
     path('signup/customer/', CustomerSignupView.as_view(), name='signup-customer'),
     path('signup/driver/', DriverSignupView.as_view(), name='signup-driver'),
     path('customer/profile/', CustomerDetilView.as_view(), name='customer-profile'),
     path('driver/profile/', DriverDetilView.as_view(), name='driver-profile'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- sts/views.py
+++ sts/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth.password_validation import validate_password
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from .serializers import ProductSerializer , DriverRequestSerializer , CustomerSerializer , DriverSrializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
 
@@ -36,10 +37,10 @@ class ProductDetilView(APIView):
     
     def patch(self,request,pk):
         product = self.get_object(pk)
-        serializer = ProductSerializer(product ,data=request.data)
+        serializer = ProductSerializer(product ,data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer._errors, status= 200)
+            return Response(serializer.data, status=200)
         return Response(serializer.errors, status=400)
     
     def delete(self, request, pk):
@@ -47,6 +48,34 @@ class ProductDetilView(APIView):
         product.delete()
         return Response(status=204)    
     
+    
+class DriverRequestListCreateView(APIView):
+    def get(self, request):
+        requests = DriverRequest.objects.all()
+        serializer = DriverRequestSerializer(requests, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = DriverRequestSerializer(data=request.data)
+
+        if serializer.is_valid():
+            product_id = request.data.get('product')
+
+            try:
+                product = Product.objects.get(id=product_id)
+             # From stack overflow `https://stackoverflow.com/questions/52575523/how-to-capture-the-model-doesnotexist-exception-in-django-rest-framework?utm_source=chatgpt.com`                
+            except Product.DoesNotExist:
+                return Response({"error": "Product not found."}, status=404)
+
+            if product.status != "Pending":
+                return Response({"error": "Product is already taken."}, status=400)
+            driver_request = serializer.save()
+            product.status = "Approved"
+            product.save()
+
+            return Response(serializer.data, status=201)
+
+        return Response(serializer.errors, status=400)
     
 class DriverRequestDetailView(APIView):
     def get_object(self, pk):
@@ -59,9 +88,24 @@ class DriverRequestDetailView(APIView):
 
     def patch(self, request, pk):
         driver_request = self.get_object(pk)
-        serializer = DriverRequestSerializer(driver_request, data=request.data)
+        serializer = DriverRequestSerializer(driver_request, data=request.data , partial=True)
         if serializer.is_valid():
             serializer.save()
+            
+            # From DRF docs: `https://www.django-rest-framework.org/api-guide/serializers/#overriding-save-instances`
+            new_status = serializer.validated_data.get('status')
+            if new_status:
+                product = driver_request.product
+
+                if new_status == 'Accepted':
+                    product.status = 'Approved'
+                elif new_status == 'PickedUp':
+                    product.status = 'PickedUp'
+                elif new_status == 'Stored':
+                    product.status = 'Stored'
+
+                product.save()
+            
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
@@ -89,6 +133,15 @@ class CustomerSignupView(APIView):
         user = User.objects.create_user(username=username, email=email, password=password)
         Customer.objects.create(user=user, full_name=full_name, address=address, phone_number=phone_number)   
         
+        tokens = RefreshToken.for_user(user)
+        return Response(
+            {
+                'refresh': str(tokens),
+                'access': str(tokens.access_token)
+            },
+            status=201
+        ) 
+        
 class DriverSignupView(APIView):
     
     def post(self, request):
@@ -106,6 +159,15 @@ class DriverSignupView(APIView):
 
         user = User.objects.create_user(username=username, email=email, password=password)
         Driver.objects.create(user=user, full_name=full_name, vehicle_type=vehicle_type, phone_number=phone_number)
+        
+        tokens = RefreshToken.for_user(user)
+        return Response(
+            {
+                'refresh': str(tokens),
+                'access': str(tokens.access_token)
+            },
+            status=201
+        )
         
         
 class CustomerDetilView(APIView):      
